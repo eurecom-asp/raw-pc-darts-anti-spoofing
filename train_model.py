82045d0dda0436d6a8db9c3e052ab8fb33c3a0ce
Wanying Ge <gewanying@outlook.com>
2022-03-16 10:43:17 +0100

    Add comment

--- train_model.py
+++ train_model.py
@@ -25,12 +25,12 @@ if __name__ == '__main__':
                     help='location of the data corpus')          
     parser.add_argument('--valid_freq', type=int, default=1, help='validate frequency')
     parser.add_argument('--report_freq', type=int, default=1000, help='report frequency in training')
-    parser.add_argument('--layers', type=int, default=8)
-    parser.add_argument('--init_channels', type=int, default=64)
-    parser.add_argument('--gru_hsize', type=int, default=1024)
-    parser.add_argument('--gru_layers', type=int, default=3)
-    parser.add_argument('--sinc_scale', type=str, default='mel', help='the ytpe of sinc layer')
-    parser.add_argument('--sinc_kernel', type=int, default=128)
+    parser.add_argument('--layers', type=int, default=8, help='number of cells of the network')
+    parser.add_argument('--init_channels', type=int, default=64, help='number of the initial channels of the network')
+    parser.add_argument('--gru_hsize', type=int, default=1024, help='number of the features in the hidden state of gru layers')
+    parser.add_argument('--gru_layers', type=int, default=3, help='number of gru layers of the network')
+    parser.add_argument('--sinc_scale', type=str, default='mel', help='the type of sinc layer')
+    parser.add_argument('--sinc_kernel', type=int, default=128, help='kernel size of sinc layer')
     parser.add_argument('--trainable', dest='is_trainable', action='store_true', help='whether using trainable sinc layer')
     parser.add_argument('--pre_trained', type=str, default=None)
     parser.add_argument('--arch', type=str, help='the searched architecture')
@@ -38,8 +38,8 @@ if __name__ == '__main__':
     parser.add_argument('--num_epochs', type=int, default=100)
     parser.add_argument('--no-mask', dest='is_mask', action='store_false', help='whether use freq mask')
     parser.add_argument('--sr', type=int, default=16000, help='default sampling rate')
-    parser.add_argument('--lr', type=float, default=5e-5)
-    parser.add_argument('--lr_min', type=float, default=2e-5)
+    parser.add_argument('--lr', type=float, default=5e-5, help='intial learning rate')
+    parser.add_argument('--lr_min', type=float, default=2e-5, help='mininum learning rate')
     parser.add_argument('--weight_decay', type=float, default=3e-4)
     parser.add_argument('--seed', type=int, default=None, help='random seed')
     parser.add_argument('--comment', type=str, default='EXP', help='Comment to describe the saved mdoel')
@@ -48,7 +48,9 @@ if __name__ == '__main__':
     parser.add_argument('--drop_path_prob', type=float, default=0.0, help='drop path probability')
     parser.add_argument('--rand', dest='is_rand', action='store_true', help='whether use rand start of input audio')
 
+    # by default, use fixed first 4 seconds of the original audio file, not using randomly selected 4 seconds
     parser.set_defaults(is_rand=False)
+    
     parser.set_defaults(is_mask=True)
     parser.set_defaults(is_trainable=False)
     
@@ -62,7 +64,8 @@ if __name__ == '__main__':
     fh = logging.FileHandler(os.path.join(args.comment, 'log.txt'))
     fh.setFormatter(logging.Formatter(log_format))
     logging.getLogger().addHandler(fh)
-
+    
+    # models will be saved under this path
     model_save_path = os.path.join(args.comment, 'models')
     if not os.path.exists(model_save_path):
         os.mkdir(model_save_path)
@@ -71,6 +74,8 @@ if __name__ == '__main__':
     dev_protocol = 'ASVspoof2019.LA.cm.dev.trl.txt'
 
     OUTPUT_CLASSES = 2
+    
+    # set random seed
     if args.seed:
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
@@ -85,13 +90,17 @@ if __name__ == '__main__':
     device = 'cuda'
     criterion = P2SGradLoss()
     criterion = criterion.cuda()
-
+    
+    # get the network architecture
     genotype = eval(args.arch)
+    # initialise the model
     model = Network(args.init_channels, args.layers, args, OUTPUT_CLASSES, genotype)
     model = model.to(device)
 
-
+    # to make sure never use pre trained front-end while setting it to trainable
+    # i.e., never update pre trained front-end sinc layer 
     assert not (args.pre_trained and args.is_trainable), "Warning: when using pre-trained sinc layer, these parameter should be set to not trainable."
+    # load pre trained front-end from the saved models, only parameters of the sinc layer is loaded
     if args.pre_trained:
         checkpoint = torch.load(args.pre_trained)
         model_dict = model.state_dict()
